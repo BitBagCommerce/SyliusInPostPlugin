75243c9988ed58942b4050b392eeb9d594b7e0c6
Gracjan Józefczyk <juzkos@gmail.com>
2021-09-01 14:42:12 +0200

    PSRC-328 Add spec test for ShippingExportEventListener

--- spec/EventListener/ShippingExportEventListenerSpec.php
+++ spec/EventListener/ShippingExportEventListenerSpec.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace spec\BitBag\SyliusInPostPlugin\EventListener;
 
-use BitBag\SyliusInPostPlugin\Api\SoapClientInterface;
 use BitBag\SyliusInPostPlugin\Api\WebClientInterface;
 use BitBag\SyliusInPostPlugin\EventListener\ShippingExportEventListener;
 use BitBag\SyliusShippingExportPlugin\Entity\ShippingExportInterface;
 use BitBag\SyliusShippingExportPlugin\Entity\ShippingGatewayInterface;
-use BitBag\SyliusShippingExportPlugin\Event\ExportShipmentEvent;
+use Doctrine\Persistence\ObjectManager;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Core\Model\Order;
 use Sylius\Component\Core\Model\ShipmentInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 
 final class ShippingExportEventListenerSpec extends ObjectBehavior
 {
-    function let(WebClientInterface $webClient, SoapClientInterface $soapClient): void
+    function let(ObjectManager $manager, FlashBagInterface $flashBag, WebClientInterface $webClient, Filesystem $filesystem): void
     {
-        $this->beConstructedWith($webClient, $soapClient);
+        $shippingLabelsPath = 'labels';
+        $this->beConstructedWith($manager, $flashBag, $webClient, $filesystem, $shippingLabelsPath);
     }
 
     function it_is_initializable(): void
@@ -26,44 +30,40 @@ final class ShippingExportEventListenerSpec extends ObjectBehavior
         $this->shouldHaveType(ShippingExportEventListener::class);
     }
 
-    function it_export_shipment(
-        ExportShipmentEvent $exportShipmentEvent,
-        ShippingExportInterface $shippingExport,
+    function it_exports_shipment(
+        ResourceControllerEvent $exportShipmentEvent,
         ShippingGatewayInterface $shippingGateway,
         ShipmentInterface $shipment,
         WebClientInterface $webClient,
-        SoapClientInterface $soapClient,
+        ShippingExportInterface $shippingExport,
         Order $order
     ): void {
-        $webClient->setShippingGateway($shippingGateway);
-
-        $shippingGateway->getCode()->willReturn(ShippingExportEventListener::INPOST_POINT_SHIPPING_GATEWAY_CODE);
+        $shippingGateway->getCode()->willReturn(ShippingExportEventListener::INPOST_SHIPPING_GATEWAY_CODE);
         $shippingGateway->getConfigValue('wsdl')->willReturn('wsdl');
 
-//        $webClient->getRequestData()->willReturn([]);
         $webClient->setShippingGateway($shippingGateway)->shouldBeCalled();
-//        $webClient->setShipment($shipment)->shouldBeCalled();
+        $webClient->createShipment($shipment)->shouldBeCalled();
+        $shippingExport->setExternalId("10")->shouldBeCalled();
+        $webClient->getLabels([10])->shouldBeCalled();
+        $shipment->getId()->shouldBeCalled();
+        $shippingExport->setLabelPath("labels/_.pdf")->shouldBeCalled();
+        $shippingExport->setState('exported')->shouldBeCalled();
 
+        /** @var \DateTime $date */
+        $date = Argument::type(\DateTime::class);
+        $shippingExport->setExportedAt($date)->shouldBeCalled();
+
+        $exportShipmentEvent->getSubject()->willReturn($shippingExport);
+        $shippingExport->getShippingGateway()->willReturn($shippingGateway);
         $shippingExport->getShipment()->willReturn($shipment);
 
-        $exportShipmentEvent->getShippingExport()->willReturn($shippingExport);
-        $exportShipmentEvent->addSuccessFlash()->shouldBeCalled();
-        $exportShipmentEvent->exportShipment()->shouldBeCalled();
-        $exportShipmentEvent->saveShippingLabel('', 'pdf')->shouldBeCalled();
-        $shippingExport->getShippingGateway()->willReturn($shippingGateway);
+        $webClient->createShipment($shipment)->willReturn(['id' => 10]);
 
-        $order->getNumber()->willReturn(1000);
+        $webClient->getShipmentById(10)->willReturn(['status' => WebClientInterface::CONFIRMED_STATUS, 'id' => 10]);
+        $webClient->getLabels()->willReturn('Label content');
         $shipment->getOrder()->willReturn($order);
-
-        $soapClient->createShipment([], 'wsdl')->willReturn(
-            (object) ['createShipmentResult' => (object) ['label' => (object) [
-                'labelContent' => '',
-                'labelType' => 't',
-            ],
-            ],
-            ]
-        );
 
         $this->exportShipment($exportShipmentEvent);
     }
+
 }
