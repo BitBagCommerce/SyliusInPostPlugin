3fdd4459c59cfe7d2994358dcd6a7629189bad13
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2023-04-27 13:08:33 +0200

    add sylius 1.12 support - fix an issue with build

--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -38,6 +38,7 @@ Encore.setOutputPath('public/build/admin/')
 
 const adminConfig = Encore.getWebpackConfig();
 
+adminConfig.resolve.alias['chart.js/dist/Chart.min'] = path.resolve(__dirname, 'node_modules/chart.js/dist/chart.min.js');
 adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
 adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
