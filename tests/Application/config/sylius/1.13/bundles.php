6aeaea4dae83efd2e991602cfbffcdb783cd7af7
Tomasz Kaliński <tomkalon@gmail.com>
2024-07-15 14:55:21 +0200

    OP-374 Added Sylius 1.13 support

--- tests/Application/config/sylius/1.13/bundles.php
+++ tests/Application/config/sylius/1.13/bundles.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
